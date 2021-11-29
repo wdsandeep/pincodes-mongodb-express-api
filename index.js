07295c93d5a68d29fd156d180f40f369034f0e51
wdsandeep <wd.sandeep@gmail.com>
2021-11-29 18:40:19 +0530

    Adding cors support

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', async(req, res) => {
     res.json({ "hello": "world" });
